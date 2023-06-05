da136bf5fdd767cd1f9e96cafd10657855f3dc90
adiy55 <adiyafe55@gmail.com>
2023-06-05 17:47:50 +0300

    Add basic movement and animations to scene

--- Assets/Scripts/AssignmentScripts/PlayerController.cs
+++ Assets/Scripts/AssignmentScripts/PlayerController.cs
@@ -6,18 +6,27 @@ public class PlayerController : NetworkBehaviour
 {
     [Header("Player Components")]
     private CharacterController _controller;
-    [Header("Move Actions")]
+    [Header("Move Action")]
     [SerializeField] private InputAction moveAction;
     [SerializeField] float speed = 3f;
     private Vector2 currMovementInput;
-    private Vector3 velocity = Vector3.zero;
-
+    private Vector3 movement = Vector3.zero;
+    private bool movePressed;
+    [Header("Jump Action")]
+    [SerializeField] InputAction jumpAction;
+    private bool jumpPressed;
+    [Header("Animations")]
+    private Animator animator;
+    private int isWalkingHash;
+    private int jumpTriggerHash;
     void OnValidate()
     {
         // Provide default bindings for the input actions.
         // Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
         if (moveAction == null)
+        {
             moveAction = new InputAction(type: InputActionType.Button);
+        }
         if (moveAction.bindings.Count == 0)
         {
             moveAction.AddCompositeBinding("2DVector")
@@ -32,9 +41,34 @@ public class PlayerController : NetworkBehaviour
                 .With("Right", "<Keyboard>/D");
         }
     }
+    private void Update()
+    {
+        if (HasStateAuthority)
+        {
+            // We have to read the button status in Update, because FixedNetworkUpdate might miss it.
+            if (jumpAction.WasPressedThisFrame())
+            {
+                jumpPressed = true;
+                animator.SetTrigger(jumpTriggerHash);
+            }
+            if (moveAction.WasReleasedThisFrame())
+            {
+                movePressed = false;
+                HandleWalkAnimations();
+            }
+            else if (moveAction.WasPressedThisFrame())
+            {
+                movePressed = true;
+                HandleWalkAnimations();
+            }
+        }
+    }
     public override void Spawned()
     {
         _controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        jumpTriggerHash = Animator.StringToHash("jump");
     }
 
     public override void FixedUpdateNetwork()
@@ -46,12 +80,41 @@ public class PlayerController : NetworkBehaviour
             // NetworkTransform only synchronizes changes from the StateAuthority.             
             // If someone without StateAuthority tries to change, the change will be local, and not transmitted to other players.
         }
-
-        currMovementInput = moveAction.ReadValue<Vector2>();
-        velocity.x = currMovementInput.x * speed;
-        velocity.z = currMovementInput.y * speed;
-        _controller.Move(velocity * Runner.DeltaTime);
+        if (jumpPressed == false)
+        {
+            currMovementInput = moveAction.ReadValue<Vector2>();
+            movement.x = currMovementInput.x;
+            movement.y = 0.0f;
+            movement.z = currMovementInput.y;
+            _controller.Move(movement * speed * Runner.DeltaTime);
+        }
+        else
+        {
+            jumpPressed = false; // Reset for next iteration
+        }
     }
-    private void OnEnable() { moveAction.Enable(); }
-    private void OnDisable() { moveAction.Disable(); }
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        jumpAction.Enable();
+    }
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        jumpAction.Disable();
+    }
+    private void HandleWalkAnimations()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+
+        // Determine idle or walking state
+        if (movePressed && !isWalking)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+        else if (!movePressed && isWalking)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+    }
 }
